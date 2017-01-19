1bc561454ff3592bf657a7f8e48f8da22b8598c3
Drew Manlove <andrew.manlove@codefutures.com>
2017-01-19 13:10:24 -0700

    save

--- reinsurance_poc/reinsurance_proposal/reinsurance_proposal.go
+++ reinsurance_poc/reinsurance_proposal/reinsurance_proposal.go
@@ -11,28 +11,40 @@ import (
 
 var logger = shim.NewLogger("SimpleContractChaincode")
 
-type ReinsuranceProposalCC struct {
+type ReinsuranceRequestCC struct {
 }
 
 // TBD
-type ReinsureProposal struct {
-	ItemName 							string 	`json:"itemName"`
-	CreateDate 						uint64 	`json:"createDate,string"`
-	TotalInsuredValue 		int 		`json:"totalInsuredValue,string"`
+type ReinsuranceRequest struct {
+	ContractType					string `json:"contractType"`
+	ContractSubType				string `json:"contractSubType"`
+	AssetType							string `json:"assetType"`
+	TotalInsuredValue 		int 	 `json:"totalInsuredValue,string"`
+	AggregateLimit				int 	 `json:"aggregateLimit,string"`
+	PortfolioHash					string `json:"portfolioHash"`
+	PortfolioURL					string `json:"portfolioUrl"`
+	InExcessOf						int		 `json:"inExcessOf,string"`
+	Status								string `json:"status"`
+	Requestor							string `json:"requestor"`
+	Requestees						[]string `json:"requestees"`
 }
 
-func (t *ReinsuranceProposalCC) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	return nil, errors.New("No Init Implementation")
+func (t *ReinsuranceRequestCC) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	logger.Debug("Init()")
+	if len(args) != 0 {
+		nil, errors.New("Init does not support arguments")
+	}
+	return nil, nil
 }
 
-func (t *ReinsuranceProposalCC) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *ReinsuranceRequestCC) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
 	logger.Debug("enter Invoke")
 	return nil, errors.New("No Invoke Implementation")
 
 }
 
-func (t *ReinsuranceProposalCC) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *ReinsuranceRequestCC) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
 	return nil, errors.New("No Query Implementation")
 }
@@ -41,7 +53,7 @@ func (t *ReinsuranceProposalCC) Query(stub shim.ChaincodeStubInterface, function
 // Main
 // ============================================================================================================================
 func main() {
-	err := shim.Start(new(ReinsureProposalCC))
+	err := shim.Start(new(ReinsuranceRequestCC))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
