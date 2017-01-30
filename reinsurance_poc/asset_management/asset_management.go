3f503a38b151a855640fae151c39d908eeb75cba
Drew Manlove <andrew.manlove@codefutures.com>
2017-01-30 12:52:36 -0700

    add v2 demo request code and asset management

---
+++ reinsurance_poc/asset_management/asset_management.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"encoding/json"
+	// "strconv"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+var logger = shim.NewLogger("AssetManagementCC")
+var assetTable = "Assets"
+
+type AssetManagementCC struct {
+}
+
+type AssetsRecord struct {
+	Submitted 		[]string	`json:"submitted"`
+	Requested			[]string	`json:"submitted"`
+	Proposed			[]string 	`json:"proposed"`
+	Accepted			[]string	`json:"accepted"`
+	Rejected			[]string	`json:"rejected"`
+}
+
+func (t *AssetManagementCC) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	logger.Debug("Init Chaincode...")
+
+	if len(args) != 0 {
+		return nil, errors.New("Init does not support arguments")
+	}
+
+	// Create enrollment table
+	err := stub.CreateTable(assetTable, []*shim.ColumnDefinition{
+		&shim.ColumnDefinition{Name: "enrollmentId", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "Records", Type: shim.ColumnDefinition_BYTES, Key: false},
+	})
+
+	if err != nil {
+		return nil, errors.New("Failed creating Assets table.")
+	}
+
+	logger.Debug("Init Chaincode finished")
+
+	return nil, nil
+}
+
+func (t *AssetManagementCC) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	logger.Debug("enter Invoke")
+	switch function {
+	case "new_request":
+			// expects ["id", "requestor", "requestees,.."]
+			if len(args) != 3 {
+				return nil, errors.New("Expects three arguments: ['id', 'requestor', 'requestees,..']")
+			}
+			return t.manage_request(stub, args)
+		default:
+			return nil, errors.New("Unrecognized Invoke function: " + function)
+	}
+
+}
+
+func (t *AssetManagementCC) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	switch function {
+	case "get_assets_record":
+			return t.get_assets_record(stub)
+		default:
+			return nil, errors.New("Unrecognized function : " + function)
+	}
+}
+
+func (t *AssetManagementCC) manage_request(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	requestId := args[0]
+	requestor := args[1]
+	requestees := strings.Split(args[0], ",")
+
+	record, err := t.get_or_create_record(stub, requestor)
+	if err != nil {
+		return nil, err
+	}
+	record.Submitted = append(record.Submitted, requestId)
+	_, err = t.save_record(stub, record, requestor)
+	if err != nil {
+		logger.Error(err)
+		return nil, errors.New("Failed to save record for id " + requestor)
+	}
+
+	for _,element := range requestees {
+		record, err := t.get_or_create_record(stub, element)
+		if err != nil {
+			return nil, err
+		}
+		record.Requested = append(record.Submitted, requestId)
+		_, err = t.save_record(stub, record, element)
+		if err != nil {
+			logger.Error(err)
+			return nil, errors.New("Failed to save record for id " + requestor)
+		}
+	}
+
+	return nil, err
+}
+
+func (t *AssetManagementCC) save_record(stub shim.ChaincodeStubInterface, record AssetsRecord, enrollmentId string) (bool, error) {
+	return false, errors.New("save_record() not implemented()")
+	recordBytes, err := json.Marshal(record)
+	if err != nil {
+		logger.Error(err)
+		return false, errors.New("Failed to serialize record")
+	}
+
+	existing, err := t.get_record(stub, enrollmentId)
+	if err != nil {
+		logger.Error(err)
+		return false, errors.New("Failed to get record for enrollment id : " + enrollmentId)
+	}
+
+	if len(existing.Columns) == 0 {
+		return stub.InsertRow(assetTable, shim.Row{
+			Columns: []*shim.Column{
+				&shim.Column{Value: &shim.Column_String_{String_: enrollmentId}},
+				&shim.Column{Value: &shim.Column_Bytes{Bytes: []byte(recordBytes)}}},
+		})
+
+	} else {
+		return stub.ReplaceRow(assetTable, shim.Row{
+			Columns: []*shim.Column{
+				&shim.Column{Value: &shim.Column_String_{String_: enrollmentId}},
+				&shim.Column{Value: &shim.Column_Bytes{Bytes: []byte(recordBytes)}}},
+		})
+	}
+}
+
+func (t *AssetManagementCC) get_or_create_record(stub shim.ChaincodeStubInterface, enrollmentId string) (AssetsRecord, error) {
+	var r AssetsRecord
+
+	existing, err := t.get_record(stub, enrollmentId)
+
+	if len(existing.Columns) == 0 {
+		r = AssetsRecord {
+			Submitted: make([]string, 0),
+			Requested: make([]string, 0),
+			Proposed: make([]string, 0),
+			Accepted: make([]string, 0),
+			Rejected: make([]string, 0),
+		}
+
+	} else {
+		err = json.Unmarshal(existing.Columns[1].GetBytes(), &r)
+		if err != nil {
+			logger.Error(err)
+			return r, errors.New("Failed to deserialize asset record: " + enrollmentId)
+		}
+	}
+
+	return r, nil
+}
+
+func (t *AssetManagementCC) get_assets_record(stub shim.ChaincodeStubInterface) ([]byte, error) {
+	bytes, err := stub.ReadCertAttribute("enrollmentId")
+	if err != nil {
+		logger.Error(err)
+		return nil, errors.New("failed to get enrollmentId attribute")
+	}
+	enrollmentId := string(bytes)
+
+	row, err := t.get_record(stub, enrollmentId)
+	if err != nil {
+		return nil, err
+	}
+
+	return row.Columns[1].GetBytes(), nil
+
+}
+
+func (t *AssetManagementCC) get_record(stub shim.ChaincodeStubInterface, enrollmentId string) (shim.Row, error) {
+	var columns []shim.Column
+	col1 := shim.Column{Value: &shim.Column_String_{String_: enrollmentId}}
+	columns = append(columns, col1)
+	return stub.GetRow(assetTable, columns)
+}
+
+
+// ============================================================================================================================
+// Main
+// ============================================================================================================================
+func main() {
+	err := shim.Start(new(AssetManagementCC))
+	if err != nil {
+		fmt.Printf("Error starting AssetManagementCC: %s", err)
+	}
+}
---
+++ reinsurance_poc/reinsurance_request/reinsurance_request.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"encoding/json"
+	// "strconv"
+	"strings"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/core/util"
+
+)
+
+var logger = shim.NewLogger("ReinsuranceRequestCC")
+var assetManagementCCId = ""
+
+type ReinsuranceRequestCC struct {
+}
+
+// TBD
+type ReinsuranceRequest struct {
+	Id 										string 		`json:"id"`
+	PortfolioSHA					string 		`json:"portfolioSha"`
+	PortfolioURL					string 		`json:"portfolioUrl"`
+	Status								string 		`json:"status"`
+	Requestor							string 		`json:"requestor"`
+	Requestees						[]string 	`json:"requestees"`
+	ContractText					string		`json:"contractText"`
+}
+
+func (t *ReinsuranceRequestCC) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	logger.Debugf("enter Init, function: [%s], args [%s]", function, args)
+
+	switch function {
+		case "init":
+			if len(args) != 1 {
+				return nil, errors.New("Expects chaincode id for asset_management as init arg")
+			}
+			assetManagementCCId = args[0]
+			return nil, nil
+		default:
+			return nil, errors.New("Unrecognized Init function: " + function)
+	}
+	return nil, nil
+}
+
+func (t *ReinsuranceRequestCC) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	logger.Debugf("enter Invoke, function: [%s], args [%s]", function, args)
+	switch function {
+		case "submit":
+			return t.submit(stub, args)
+		default:
+			return nil, errors.New("Unrecognized Invoke function: " + function)
+	}
+}
+
+func (t *ReinsuranceRequestCC) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	logger.Debugf("enter Query, function: [%s], args [%s]", function, args)
+	switch function {
+		case "get_request":
+			if len(args) != 1 {
+				return nil, errors.New("Expected 1 arg, id") // TODO temporary
+			}
+			return stub.GetState(args[0])
+		default:
+			return nil, errors.New("Unrecognized Invoke function: " + function)
+	}
+}
+
+func (t *ReinsuranceRequestCC) submit(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	logger.Debug("submit()")
+
+	id := "1" // TODO
+	requestees := strings.Split(args[0], ",")
+	portfolioSha := args[1]
+	portfolioUrl := args[2]
+	contractText := args[3]
+	status := "requested"
+	bytes, err := stub.ReadCertAttribute("enrollmentId")
+	if err != nil {
+		logger.Error(err)
+		return nil, errors.New("failed to get enrollmentId attribute")
+	}
+	requestor := string(bytes)
+
+	rr := ReinsuranceRequest {
+		Id: id,
+		Requestor: requestor,
+		Requestees: requestees,
+		PortfolioSHA: portfolioSha,
+		PortfolioURL: portfolioUrl,
+		ContractText: contractText,
+		Status: status,
+	}
+
+	// Submit
+	bytes, err = json.Marshal(rr)
+	if err != nil {
+		logger.Error(err)
+		return nil, errors.New("Failed to serialize request")
+	}
+
+	err = stub.PutState(id, bytes)
+	if err != nil {
+		logger.Error(err)
+		return nil, errors.New("Failed to submit request")
+	}
+
+	// Note with asset management
+	// ["id", "requestor", "requestees,.."]
+	invokeArgs := util.ToChaincodeArgs("new_request", id, requestor, strings.Join(requestees,","))
+	response, err := stub.QueryChaincode(assetManagementCCId, invokeArgs)
+
+	// TODO
+	logger.Debugf("Enrollment service response is %s", string(response))
+
+
+	return nil, nil
+}
+
+
+
+// ============================================================================================================================
+// Main
+// ============================================================================================================================
+func main() {
+	err := shim.Start(new(ReinsuranceRequestCC))
+	if err != nil {
+		fmt.Printf("Error starting ReinsuranceRequestCC: %s", err)
+	}
+}
