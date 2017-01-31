960ef8f6a068aac4e14131fe41b894df65455d42
Drew Manlove <andrew.manlove@codefutures.com>
2017-01-31 14:25:08 -0700

    save

--- reinsurance_poc/reinsurance_proposal/reinsurance_proposal.go
+++ reinsurance_poc/reinsurance_proposal/reinsurance_proposal.go
@@ -25,7 +25,7 @@ func (t *ReinsuranceProposalCC) Init(stub shim.ChaincodeStubInterface, function 
 	logger.Debug("Init()")
 
 	bytes, err := stub.GetPayload()
-	var spec pb.ChaincodeInvocationSpec
+	var spec pb.ChaincodeInput
 	err = proto.Unmarshal(bytes, &spec)
 
 	if err != nil {
